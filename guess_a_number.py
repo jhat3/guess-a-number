1e713127f89530b8957c5500a4f91f9067baf15a
Hatley <jhatle1824@greenville.k12.sc.us>
2017-09-15 09:25:06 -0400

    Can't use words

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,13 +1,32 @@
 import random
 
-high=1000000
+#config
+high=100
 low=1
+limit=10
+
+#start game
 rand = random.randrange(low,high)
 print("I'm thinking of a number from " + str(low) + "to " + str(high) + ".");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+#helper functions
+def get_guess():
+    while True:
+        g=input("Take a guess: ")
+
+        if g.isnumeric():
+            g=int(g)
+            return g
+        else:
+            print("You must enter a number")
+
+#play the game
+while guess != rand and tries < limit:
+    guess = get_guess()
+    
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -15,7 +34,12 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
 
-print("Game over")
+    tries += 1
+
+#tell player outcome
+if guess == rand:
+    print("You win!")
+
+else:
+    print("You lose. I was thinking of " +str(rand)+ ".")
